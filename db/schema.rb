9f1fb90950a532ea721457fb82949570a81da895
Sam <sam-rolfe@hotmail.com>
2024-09-05 21:30:41 +1000

    merged

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_28_223908) do
 
+
   create_table "activity_types", charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "abbreviation", null: false
